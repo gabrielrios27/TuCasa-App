41b12344557aa974b23388c74daa592ecba15430
gabrielrios27 <juangrios27@gmail.com>
2022-12-12 20:00:13 -0300

    Feat - Crea reducers

---
+++ src/store/reducers/index.js
@@ -0,0 +1,3 @@
+export { default as floorProductsReducer } from './floorProducts.reducer';
+export { default as floorCategoryReducer } from './floorCategory.reducer';
+export { default as wallCoveringCategoryReducer } from './wallCoveringCategory.reducer';
