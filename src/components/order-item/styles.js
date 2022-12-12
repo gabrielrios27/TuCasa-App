bfe4603a0a6dd77d4dc5d3c124d725b97faec44a
gabrielrios27 <juangrios27@gmail.com>
2022-12-12 17:56:47 -0300

    Feat - Crea order item y en utils la funcion para obtener la fecha

---
+++ src/components/order-item/styles.js
@@ -0,0 +1,39 @@
+import { COLORS } from '../../constants/themes/colors';
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 10,
+    borderBottomColor: COLORS.primaryDark,
+    borderBottomWidth: 1,
+    backgroundColor: COLORS.primary,
+    height: 85,
+    marginHorizontal: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+    justifyContent: 'center',
+  },
+  headerContainer: {},
+  date: {},
+  contentContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  content: {},
+  total: {
+    fontSize: 16,
+    fontFamily: 'SourceSansPro-Bold',
+  },
+});
